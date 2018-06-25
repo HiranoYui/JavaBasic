50c8f31db05c3c04c9f91f0ede18795274d36f86
DESKTOP-HBCA6N8\yuiki <yui.kitei@gmail.com>
2018-06-25 15:07:36 +0900

    11-2~13-3

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -1,4 +1,5 @@
 package practice11;
+
 /*
  * PTra11_02.java
  *   作成	LIKEIT	2017
@@ -10,11 +11,29 @@ public class PTra11_02 {
 	public static void main(String[] args) {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
+		 FileReaderClass.readBookDataFile();
+		 Book[]books= FileReaderClass.readBookDataFile();
+
+		 java.util.Scanner scanner = new java.util.Scanner(System.in);
+		 System.out.println("探したい本のタイトル（又はその一部）を入力してください");
+
+		 String line = scanner.nextLine();
+
+		 for(int i=0;i<books.length;i++) {
+			 books[i].title.contains(line);
+
+			 if(books[i].title.contains(line))
+			 System.out.println(books[i].dispBookInfo());
 
 
-		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
+		 }
 
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
 
-	}
+
+        }
+
+
+
+
 }
--- Practice/src/practice13/ptra13/PTra13_03.java
+++ Practice/src/practice13/ptra13/PTra13_03.java
@@ -5,6 +5,7 @@
  * Copyright(c) Rhizome Inc. All Rights Reserved.
  */
 package practice13.ptra13;
+import practice13.common.Slime;
 
 public class PTra13_03 {
 
@@ -13,14 +14,19 @@ public class PTra13_03 {
 	 * 	●HP, POWER, ENDURANCEはそれぞれ、（10, 5, 2）
 	 */
 
+
+
 	public static void main(String[] args) {
 		// ★ common.Slimeクラスをインスンタンス化してください
+		Slime slime=new Slime();
 
 
 		// ★ common.Slimeインスタンスのnameに"スライム"を設定して下さい
+		slime.setName("スライム");
 
 
 		// ★ common.Slimeインスタンスのキャラクター情報を表示してください
+		System.out.println(slime.showParameter());
 
 	}
 }
